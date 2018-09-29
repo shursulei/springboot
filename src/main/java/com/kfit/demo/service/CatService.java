1f98eef044e1f8041df8648b4b531a4d89cf73ed
shursulei <1368455669@qq.com>
2018-09-29 14:46:28 +0800

    springboot-hello-world1入门代码

---
+++ src/main/java/com/kfit/demo/service/CatService.java
@@ -0,0 +1,45 @@
+package com.kfit.demo.service;
+
+import javax.annotation.Resource;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.kfit.demo.bean.Cat;
+import com.kfit.demo.dao.CatDao;
+import com.kfit.demo.repository.CatRepository;
+
+@Service
+public class CatService {
+	@Resource
+	private CatRepository catRepository;
+	
+	@Resource
+	private CatDao catDao;
+	/**
+	 * save,update ,delete 方法需要绑定事务.
+	 * 
+	 * 使用@Transactional进行事务的绑定.
+	 * 
+	 * @param cat
+	 */
+	//保存数据.
+	@Transactional
+	public void save(Cat cat){
+		catRepository.save(cat);
+	}
+		
+	//删除数据
+	@Transactional
+	public void delete(int id){
+		catRepository.delete(id);
+	}
+	
+	//查询数据.
+	public Iterable<Cat> getAll(){
+		return catRepository.findAll();
+	}
+	public Cat selectByCatName(String catName){
+		return catDao.selectByCatName(catName);
+	}
+}
